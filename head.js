0f7d86deccb02bd4904c223587206c6b81814f7a
viacaelestis <affectuminflamma@gmail.com>
2023-02-06 22:13:55 -0700

    head practice

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊 Assertion Passed: ${actual} === ${expected}`);
+  }  else {
+      console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  const head = function(array) {
+    return array[0];
+  };
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
