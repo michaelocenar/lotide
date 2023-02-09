5983bc317d73f55cdf63d3f6327b20f1694d059b
viacaelestis <affectuminflamma@gmail.com>
2023-02-08 23:57:12 -0700

    made letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i].toLowerCase();
+    if (letter >= 'a' && letter <= 'z') {
+      if (letterPositions[letter]) {
+        letterPositions[letter].push(i);
+      } else {
+        letterPositions[letter] = [i];
+      }
+    }
+  }
+
+  return results;
+};
