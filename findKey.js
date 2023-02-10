8dc66cf6951d94d2e02f6e088711d6cc35240182
viacaelestis <affectuminflamma@gmail.com>
2023-02-09 20:00:17 -0700

    made findKey.js

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊 Assertion Passed: ${actual} === ${expected}`);
+  }  else {
+      console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+  const findKey = function(object, callback) {
+    for (let key in object) {
+      if (callback(object[key])) {
+        return key;
+      }
+    }
+    return undefined;
+  };
+  
+
+  const data = {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  };
+  
+  assertEqual(findKey(data, x => x.stars === 2), "noma");
+  assertEqual(findKey(data, x => x.stars === 3), "Akaleri");
+  assertEqual(findKey(data, x => x.stars === 4), undefined);
+  assertEqual(findKey(data, x => x.stars === 2), "Ora")
+  assertEqual(findKey({}, x => x), undefined);
+  assertEqual(findKey({ a: 1, b: 2, c: 3 }, x => x === 2), "b");
