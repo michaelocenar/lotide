f8f8c7706d6eb171ace33740ae32c1434149c8b7
viacaelestis <affectuminflamma@gmail.com>
2023-02-12 09:15:48 -0700

    made 2 new test files for Assert Equal and head.js

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😊 Assertion Passed: ${actual} === ${expected}`);
-  }  else {
-      console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = require('./assertEqual');
+
   const head = function(array) {
     return array[0];
   };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- middle.js
+++ middle.js
@@ -18,9 +18,9 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const middle = function(array) {
-  let middleIndex = Math.floor(array.length / 2);
+  const middleIndex = Math.floor(array.length / 2);
   if (array.length % 2 === 0) {
-    return [array[middleIndex - 1], arr[middleIndex]];
+    return [array[middleIndex - 1], array[middleIndex]];
   } else {
     return array[middleIndex];
   }
